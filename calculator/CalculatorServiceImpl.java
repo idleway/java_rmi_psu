f7fee10dd91421e5efce2a6dc8f813d658420b0f
idleway <rtrtr82@gmail.com>
2022-03-27 20:22:52 +0500

    fix server

--- calculator/CalculatorServiceImpl.java
+++ calculator/CalculatorServiceImpl.java
@@ -1,23 +1,8 @@
 package calculator;
 
-public class CalculatorServiceImpl implements Calculator {     
+public class CalculatorServiceAddImpl implements Calculator {     
     @Override
     public double add(double a, double b){
         return a + b;
-    }
-
-    @Override
-    public double sub(double a, double b){
-        return a - b;
-    }
-
-    @Override
-    public double mul(double a, double b){
-        return a * b;
-    }
-
-    @Override
-    public double div(double a, double b){
-        return a / b;
     }
 }
