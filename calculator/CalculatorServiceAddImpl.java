ac4cdff0f2658d0c08eb1a94fd6ee71d9e8a3b4a
idleway <rtrtr82@gmail.com>
2022-03-27 20:27:00 +0500

    fix server

--- calculator/CalculatorServiceAddImpl.java
+++ calculator/CalculatorServiceAddImpl.java
@@ -5,4 +5,19 @@ public class CalculatorServiceAddImpl implements Calculator {
     public double add(double a, double b){
         return a + b;
     }
+
+    @Override
+    public double sub(double a, double b){
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public double mul(double a, double b){
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public double div(double a, double b){
+        throw new NotImplementedException();
+    }
 }
