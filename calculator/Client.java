022a281e3fb0f693df2645c8ea7dc53894dbf5a0
idleway <rtrtr82@gmail.com>
2022-03-27 20:09:09 +0500

    fix client

--- calculator/Client.java
+++ calculator/Client.java
@@ -6,11 +6,12 @@ import java.rmi.registry.Registry;
 public class Client {    
 
     public static void main(String[] args) {
-        int registryPort = 2233;
-        Registry registry = LocateRegistry.getRegistry(registryPort);
-        Calculator calc = (Calculator) registry.lookup("CalculatorService");
-
+        int registryPort = 2233;        
         try{
+            
+            Registry registry = LocateRegistry.getRegistry(registryPort);
+            Calculator calc = (Calculator) registry.lookup("CalculatorService");
+
             System.out.println(calc.add(7,3));
             System.out.println(calc.sub(7,3));
             System.out.println(calc.mul(7,3));
