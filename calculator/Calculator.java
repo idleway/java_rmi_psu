bb205aaf56db423301d672d8c0de7c5e9db6cf8a
idleway <rtrtr82@gmail.com>
2022-03-27 20:35:15 +0500

    fix server exception

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -5,8 +5,8 @@ import java.rmi.RemoteException;
 
 
 public interface Calculator extends Remote {
-    double add(double a, double b) throws Exception
-    double sub(double a, double b) throws Exception
+    double add(double a, double b) throws Exception;
+    double sub(double a, double b) throws Exception;
     double mul(double a, double b) throws Exception;
     double div(double a, double b) throws Exception;
     }
