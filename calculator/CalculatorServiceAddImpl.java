a5e7c1b5018d5502adbb3f21cd3db16b7b92b2ac
idleway <rtrtr82@gmail.com>
2022-03-27 20:33:01 +0500

    fix server exception

--- calculator/CalculatorServiceAddImpl.java
+++ calculator/CalculatorServiceAddImpl.java
@@ -2,22 +2,22 @@ package calculator;
 
 public class CalculatorServiceAddImpl implements Calculator {     
     @Override
-    public double add(double a, double b) throws Exception{
+    public double add(double a, double b){
         return a + b;
     }
 
     @Override
-    public double sub(double a, double b) throws Exception{
+    public double sub(double a, double b){
         throw new Exception("Not Implemented");
     }
 
     @Override
-    public double mul(double a, double b) throws Exception{
+    public double mul(double a, double b){
         throw new Exception("Not Implemented");
     }
 
     @Override
-    public double div(double a, double b) throws Exception{
+    public double div(double a, double b){
         throw new Exception("Not Implemented");
     }    
 }
