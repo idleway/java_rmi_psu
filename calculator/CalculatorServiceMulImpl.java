b7b8e57904249b6d812463deea5f5288fbdb9678
idleway <rtrtr82@gmail.com>
2022-03-27 20:44:44 +0500

    4 different services

---
+++ calculator/CalculatorServiceMulImpl.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class CalculatorServiceMulImpl implements Calculator {     
+    @Override
+    public double add(double a, double b) throws Exception{
+        throw new Exception("Not Implemented");
+    }
+
+    @Override
+    public double sub(double a, double b) throws Exception{
+        throw new Exception("Not Implemented");
+    }
+
+    @Override
+    public double mul(double a, double b) throws Exception{
+        return a * b;        
+    }
+
+    @Override
+    public double div(double a, double b) throws Exception{
+        throw new Exception("Not Implemented");
+    }    
+}
